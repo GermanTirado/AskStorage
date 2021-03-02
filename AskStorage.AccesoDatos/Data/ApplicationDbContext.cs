cadb55fac2656fdd7eb83f14b85024e01fe90155
German <germanalonso05@gmail.com>
2021-03-02 10:22:27 -0700

    Primer commit

---
+++ AskStorage.AccesoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using AskStorage.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AskStorage.AccesoDatos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categoria { get; set; }
+    }
+}
---
+++ AskStorage.AccesoDatos/Data/Repository/IContenedorTrabajo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AskStorage.AccesoDatos.Data.Repository
+{
+    public interface IContenedorTrabajo : IDisposable
+    {
+        // Repositorios
+        ICategoriaRepository Categoria { get; }
+
+        void Save();
+    }
+}
